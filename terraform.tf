1552fc5aa24c608003177eca21016d27c0acf7bb
Varsha S <110251631+VarshaRajS@users.noreply.github.com>
2024-10-11 15:08:51 +0530

    Update terraform.tf

--- terraform.tf
+++ terraform.tf
@@ -1,3 +1,9 @@
+variable "region" {
+  description = "The AWS region to deploy resources."
+  type        = string
+  default     = "us-east-1"  
+}
+
 terraform {
   required_providers {
     aws = {
