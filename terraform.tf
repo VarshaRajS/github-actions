6956519126e4394e89076df1c2d1734d1091d101
Varsha S <110251631+VarshaRajS@users.noreply.github.com>
2024-10-11 14:37:02 +0530

    Update terraform.tf

--- terraform.tf
+++ terraform.tf
@@ -7,11 +7,13 @@ terraform{
   }
 }
 
+# configure aws provider
 provider "aws" {
   region = "us-east-1"
 }
 
-resource "aws_s3_bucket" "example" {
+# creating the s3 bucket
+resource "aws_s3_bucket" "my_bucket" {
   bucket = "my-test-bucket"
 
   tags = {
@@ -24,3 +26,35 @@ resource "aws_s3_bucket" "example" {
     index_document = "index.html"
   }
 }
+
+# creating and attaching bucket ploicy
+resource "aws_s3_bucket_policy" "my_bucket_policy" {
+bucket = aws_s3_bucket.my_bucket.id
+
+policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Sid      = "PublicReadGetObject"
+        Effect   = "Allow"
+        Principal = "*"
+        Action   = "s3:GetObject"
+        Resource = "${aws_s3_bucket.my_bucket.arn}/*"
+      }
+    ]
+  })
+}
+
+# Output the bucket website URL
+output "bucket_website_url" {
+  value = aws_s3_bucket.my_bucket.website_endpoint
+}
+
+
+
+
+
+
+
+
+
