e4b9a662550a10902fa1d4c6b1c582816b1cbbbe
Varsha S <110251631+VarshaRajS@users.noreply.github.com>
2024-10-12 21:35:56 +0530

    Update terraform.tf

--- terraform.tf
+++ terraform.tf
@@ -37,6 +37,16 @@ resource "aws_s3_bucket_website_configuration" "my_bucket_website" {
   }
 }
 
+# Disable Block Public Access
+resource "aws_s3_bucket_public_access_block" "my_bucket_public_access_block" {
+  bucket = aws_s3_bucket.my_bucket.id
+
+  block_public_acls       = false
+  block_public_policy     = false
+  ignore_public_acls      = false
+  restrict_public_buckets = false
+}
+
 # Create and attach bucket policy
 resource "aws_s3_bucket_policy" "my_bucket_policy" {
   bucket = aws_s3_bucket.my_bucket.id
