f2429e1d052a984acdd4b31d6c9b0660a87789e6
Varsha S <110251631+VarshaRajS@users.noreply.github.com>
2024-10-20 13:58:52 +0530

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -2,7 +2,7 @@ terraform {
   backend "s3" {
     bucket         = "varsha-terraform-states"
     key            = "varsha/terraform.tfstate"
-    region         = "us-west-2"
+    region         = "us-west-1"
    dynamodb_table = "terraform-lock"
   }
   required_version = ">= 0.12"
