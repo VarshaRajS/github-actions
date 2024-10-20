5526094e3de1ec7d9b5af941d765c4388b65aa56
Varsha S <110251631+VarshaRajS@users.noreply.github.com>
2024-10-20 14:07:35 +0530

    Update terraform.tf

--- terraform.tf
+++ terraform.tf
@@ -74,29 +74,6 @@ resource "aws_s3_bucket_policy" "my_bucket_policy" {
   })
 }
 
-# Create the s3 bucket for backend to store tfstate file
-
-resource "aws_s3_bucket" "terraform_state" {
-  bucket = "varsha-terraform-states" 
-
-  versioning {
-    enabled = true
-  }
-}
-
-# Create the dynamodb table for state locking
-
-resource "aws_dynamodb_table" "terraform_lock" {
-  name         = "terraform-lock"
-  billing_mode = "PAY_PER_REQUEST"
-  hash_key     = "LockID"
-
-  attribute {
-    name = "LockID"
-    type = "S"
-  }
-}
-
 # Output the bucket website URL
 output "bucket_website_url" {
   value = "http://${aws_s3_bucket.my_bucket.bucket}.s3-website-${var.region}.amazonaws.com/"
